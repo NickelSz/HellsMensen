a0ff2c4de4e304849fb7eb3f7c2c001ea343ad7f
Lauri101 <s0566403@htw-berlin.com>
2020-09-22 14:01:26 +0200

    init

--- src/HTML/mensenholen.js
+++ src/HTML/mensenholen.js
@@ -54,30 +54,64 @@ mensen_liste.onchange = function () {
       var count = 0;
       for (count in food) {
         var newentry = document.createElement("tr");
+        newentry.setAttribute("class", "tabelle");
+        newentry.setAttribute("onclick","detailAnzeigen" );
+        table.appendChild(newentry);
+
         var gericht = document.createElement("div");
         gericht.innerHTML = food[count].name;
         gericht.setAttribute("class", "food" );
+        if ( food[count].name == "Erbseneintopf mit Mettenden"){
+          var E1Eintopf = document.createElement("img");
+          E1Eintopf.setAttribute("src", "images/Erbsensuppe-Rezept-kochen-Eintopf-herzelieb-21.jpg");
+          E1Eintopf.setAttribute("id", "eintopf");
+          newentry.appendChild(E1Eintopf);
+        }
         var kategorie = document.createElement("div");
         kategorie.innerHTML = food[count].category;
-
         kategorie.setAttribute("class", "kategorie" );
-        var preis = document.createElement("div");
-        
-        preis.innerHTML = food[count].prices.students;
-        kategorie.setAttribute("class", "pricestudents" );
-
-        table.appendChild(newentry);
-        newentry.appendChild(gericht);
-        newentry.appendChild(kategorie);
-        newentry.appendChild(preis);
-
+        if ( food[count].category == "Eintopf"){
+          var kuh = document.createElement("img");
+          kuh.setAttribute("src", "images/kuh.png");
+          kuh.setAttribute("id", "kuh");
+          newentry.appendChild(kuh);
+        }
         if ( food[count].category == "Wahlmenü (Komponente ohne Fleisch)"){
           var vegi = document.createElement("img");
-          vegi.setAttribute("src", "images/cake-black-18dp.svg");
+          vegi.setAttribute("src", "images/vegetarian-mark.png");
           vegi.setAttribute("id", "vegilogo");
           newentry.appendChild(vegi);
         }
+        if ( food[count].category == "Menü"){
+          var menulog = document.createElement("img");
+          menulog.setAttribute("src", "images/menu_book-black-18dp.svg");
+          menulog.setAttribute("id", "menulog");
+          newentry.appendChild(menulog);
+        }
+        var infodetail = document.createElement("img");    
+        infodetail.setAttribute("src", "images/info-black-18dp.svg");
+        infodetail.setAttribute("id", "detailinfo");
+          
+
+        var preis = document.createElement("div");    
+        preis.innerHTML = food[count].prices.students + "€ / "+ food[count].prices.employees + "€ / "+ food[count].prices.others + "€";
+        preis.setAttribute("class", "preisstudents" );
+
+        table.appendChild(newentry);
+        newentry.appendChild(gericht);
+        newentry.appendChild(infodetail);
+        
+        newentry.appendChild(preis);    
     
       }
     });
 };
+
+function detailAnzeigen() {
+  var x = document.getElementById("showdetail");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
