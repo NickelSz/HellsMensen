498957363bc469f6d5e2c1d16946e6ace7ed013d
Lauri101 <s0566403@htw-berlin.com>
2020-09-25 08:55:01 +0200

    init

---
+++ src/HTML/map.js
@@ -0,0 +1,102 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibGF1cmlwZWMiLCJhIjoiY2tmaGF1YXJoMGF1MTJ1cDhlMHp0aDV4dCJ9.8Sz7_ZLqPFs6Pte0nhpDzQ';
+
+
+var x = document.getElementById("demo");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+ // var lat = position.coords.latitude;
+  //var long = position.coords.longitude;
+  //console.log(lat, long);
+}  
+
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v10',
+  center: [13.5135232, 52.455014399999996],
+  zoom: 10
+  
+});
+
+var geojson = {
+    type: 'FeatureCollection',
+    features: [{
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [11.6756665706635
+            , 52.1400665853041]
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Eigener Standort'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [11.6475999355316, 52.1396188273019]
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'San Francisco, California'
+      }
+    }]
+  };
+
+ 
+
+
+
+
+  // add markers to map
+geojson.features.forEach(function(marker) {
+
+    // create a HTML element for each feature
+    var el = document.createElement('div');
+    el.className = 'marker';
+  
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el)
+      .setLngLat(marker.geometry.coordinates)
+      .addTo(map);
+  });
+
+  var homepointjson = {
+    type: 'FeatureCollection',
+    features: [{
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [13.5135232, 52.455014399999996]
+      },
+      properties: {
+        title: 'Mapbox',
+        description: 'Eigener Standort'
+      }
+    }]
+  };
+
+   // add markers to map
+   homepointjson.features.forEach(function(marker) {
+
+    // create a HTML element for each feature
+    var mr = document.createElement('div');
+    mr.className = 'markerhome';
+  
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(mr)
+      .setLngLat(marker.geometry.coordinates)
+      .addTo(map);
+  });
